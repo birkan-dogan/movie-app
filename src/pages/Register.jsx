391e3b4bb0d285a813f9a75e451ca026b0855312
birkan <b.561birkan@gmail.com>
2022-07-19 21:27:07 +0300

    Register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,62 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <p>Register</p>
+    <div className="d-flex justify-content-center">
+      <div className="form-image d-none d-md-block">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Register</h1>
+        <form id="register">
+          <div className="mb-3">
+            <label htmlFor="firstName" className="form-label">
+              First Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              id="firstName"
+              placeholder="Enter Your First Name"
+              required
+            />
+            <label htmlFor="lastName" className="form-label">
+              Last Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              id="lastName"
+              placeholder="Enter Your Last Name"
+              required
+            />
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter Your Email"
+              required
+            />
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Register"
+          />
+        </form>
+      </div>
     </div>
   );
 };
