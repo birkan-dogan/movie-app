1512c7f0bf29a2854e4d07cc0fe7cb5566d16d26
birkan <b.561birkan@gmail.com>
2022-07-19 22:01:42 +0300

    Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
-    <div>
-      <p>Login</p>
+    <div className="d-flex justify-content-center">
+      <div className="form-image d-none d-md-block">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">LogIn</h1>
+        <form id="register" onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter Your Email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter Your Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="link">Forgot Password ?</div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+          />
+        </form>
+        <button className="btn btn-primary form-control">
+          Continue with Google
+        </button>
+      </div>
     </div>
   );
 };
