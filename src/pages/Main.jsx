bf2cf36e79a569f53bd02d1ce58289844b82be74
birkan <b.561birkan@gmail.com>
2022-07-20 21:57:15 +0300

    navigate data to MovieCard

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
@@ -11,7 +12,7 @@ const Main = () => {
     setLoading(true);
     axios
       .get(API)
-      .then((res) => console.log(res.data.results))
+      .then((res) => setMovies(res.data.results))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   };
@@ -25,7 +26,7 @@ const Main = () => {
           <span className="sr-only">Loading</span>
         </div>
       ) : (
-        movies?.map((movie) => null)
+        movies?.map((movie) => <MovieCard key={movie.id} {...movie} />)
       )}
     </div>
   );
