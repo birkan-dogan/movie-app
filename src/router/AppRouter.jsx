20db5691b708671b767f801c6da6ac12373e51f3
birkan <b.561birkan@gmail.com>
2022-07-20 23:09:44 +0300

    PrivateRouter logic added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { AuthContext } from "../context/AuthContext";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+  function PrivateRouter() {
+    return currentUser ? <Outlet /> : <Navigate to="/login" replace />;
+  }
   return (
-    <div>
-      <p>Router</p>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<PrivateRouter />}>
+          <Route path="" element={<MovieDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
-
 export default AppRouter;
